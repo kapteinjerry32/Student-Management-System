dd8d20a87ab9584022442a2a866fd6a235ac3706
Jeremy <601206@student.belgiumcampus.ac.za>
2024-11-08 01:00:09 +0200

    Edited Student Class with a constructor and encapsulation of variables

--- Student Management System/Student.cs
+++ Student Management System/Student.cs
@@ -8,5 +8,23 @@ namespace Student_Management_System
 {
     internal class Student
     {
+        private int studentID, age;
+        private string fullName, course;
+
+        public Student(int studentID, string fullName, int age, string course)
+        {
+            StudentID = studentID;
+            Age = age;
+            FullName = fullName;
+            Course = course;
+        }
+
+        public int StudentID { get => studentID; set => studentID = value; }
+
+        public string FullName { get => fullName; set => fullName = value; }
+        
+        public int Age { get => age; set => age = value; }
+
+        public string Course { get => course; set => course = value; }
     }
 }
